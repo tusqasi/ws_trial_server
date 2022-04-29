3c9279a36cb3582b2ebca5377bc20030313131af
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-04-29 11:02:05 +0530

    ♻ Smoll change

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 // Server
-import {WebSocketServer} from "ws";
-
-const wss = new WebSocketServer({port : 8080});
 console.log("Started")
-wss.on("connection", function connection(ws) {
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-    ws.send(JSON.stringify(JSON.parse(data)));
-  });
-});
